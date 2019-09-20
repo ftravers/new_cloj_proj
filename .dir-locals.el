d3161a99d6a5635ce98679c8e9cfb43fd8e35178
Fenton Travers <fenton.travers@gmail.com>
2019-09-20 08:46:29 -0700

    .

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((cider-default-cljs-repl . figwheel-main)
+         (cider-figwheel-main-default-options . "fe_dev")
+         (cider-clojure-cli-global-options . "-A:dev"))))
---
+++ src/new_clj_proj/core.cljs
@@ -0,0 +1,13 @@
+  (ns ds-queue.core
+    (:require
+     [rum.core :refer [defc mount] :as rum]))
+
+  (defc hello []
+    [:div "Hello!"])
+
+  (defn main-page [comp]
+    (mount
+     (comp)
+     (js/document.getElementById "app")))
+
+  (main-page hello)
